466cf63e5b039448da00da66cea6f9ac9c11451a
Michael Dunnegan <mdunnegan@spicy.io>
2013-08-24 17:04:42 -0400

    added a full_name method to User, updated the header

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+  #has_one :avatar
 
   # rails 4, throwing me a curveball
   # attr_accessible :first_name, :last_name
-  
+
+  def full_name
+  	first_name + " " + last_name
+  end
+
 end
