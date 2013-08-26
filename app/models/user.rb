a5a722496a5df7d48e35a5805a8206ba2fd90ddc
Michael Dunnegan <mdunnegan@spicy.io>
2013-08-26 12:03:25 -0400

    fixed database migrations, and header now displays the current users full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   # attr_accessible :first_name, :last_name
 
   def full_name
-  	first_name + " " + last_name
+  	self.first_name + " " + self.last_name
   end
 
 end
--- db/migrate/20130824154705_remove_name_and_add_first_and_last_name_to_user.rb
+++ db/migrate/20130824154705_remove_name_and_add_first_and_last_name_to_user.rb
@@ -1,6 +1,10 @@
+# this got fucked up
+# schema is ahead of the app
+# app data didn't get updated
+
 class RemoveNameAndAddFirstAndLastNameToUser < ActiveRecord::Migration
   def change
-  	remove_column :users, :name
+  	remove_column :users, :name, :string
   	add_column :users, :first_name, :string
   	add_column :users, :last_name, :string
   end
